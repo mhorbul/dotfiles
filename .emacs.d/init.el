4427cdd3539c692b4ba63bc529c6285d5faee630
Maksim Horbul <mhorbul@Maksims-MacBook-Pro.local>
2017-02-24 16:23:46 -0800

    replace start-kit with better-defaults package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,12 +6,8 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp
-                                  starter-kit-bindings
-                                  starter-kit-ruby
-                                  starter-kit-js
-                                  starter-kit-eshell))
+(defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous
+                                      find-file-in-project smex scpaste))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -27,13 +23,17 @@
  '(cursor-color "#5c5cff")
  '(custom-safe-themes (quote ("edb0e9dce76acf08243762d30683293812c838773f0e9f41b7e6baf904776d6c" default)))
  '(foreground-color "#5c5cff")
- '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t)))))
+ '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t))))
+ '(package-selected-packages
+   (quote
+    (scpaste smex paredit ido-ubiquitous idle-highlight-mode find-file-in-project better-defaults)))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:inherit nil :background "color-236" :foreground "Old Lace")))))
+ '(hl-line ((t (:inherit nil :background "color-236" :foreground "Old Lace"))))
+)
 
-(add-to-list 'load-path "~/.emacs.d")
-(require 'org-html5presentation)
+(add-to-list 'load-path "~/.emacs.d/mhorbul")
